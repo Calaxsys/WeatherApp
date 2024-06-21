40805a9196d464dac645371fb9cf391a40b94141
Chris <ffson1999@yahoo.com>
2024-06-21 05:15:18 -0400

    Setup fetchWeather to request data from the API

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -3,4 +3,9 @@ const BASE_URL = "http://api.weatherapi.com/v1/current.json";
 
 async function getWeather(location) {
   const URL = `${BASE_URL}?key=${API_KEY}&q=${location}`;
+  const response = await fetch(URL, { mode: "cors" });
+  const locationData = await response.json();
+  console.log(locationData.location);
 }
+
+export { getWeather };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles.css";
+import { getWeather } from "./fetchWeather";
+
+getWeather("New York");
